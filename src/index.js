e69e5e07cec63e821fbe7cf809643b50d5a1d88d
Marianela Landoni Herrera <mlandonih@hotmail.com>
2022-09-05 13:41:24 +0200

    change error 4000

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const server = express();
-server.use(cors())
-server.use(express.json())
+server.use(cors());
+server.use(express.json());
 const port = 4000;
 server.listen(port, () => {
-    console.log('listening' + port);
-})
+  console.log('listening' + port);
+});
 //endpoint
-server.post("/card", (req, res) => {
-    const responseSuccess = {
-        cardURL: "https://awesome-profile-cards.herokuapp.com/card/29111662374762259",
-        success: true
-    }
-    const responseError = {
-        success: false,
-        error: "Falta un parámetro"
-    }
-    res.json(responseError);
+server.post('/card', (req, res) => {
+  const responseSuccess = {
+    cardURL:
+      'https://awesome-profile-cards.herokuapp.com/card/29111662374762259',
+    success: true,
+  };
+  const responseError = {
+    success: false,
+    error: 'Falta un parámetro',
+  };
+  res.json(responseError);
 });
 // endpoint "devolver tarjeta"
-server.get("/card/:id", (req, res) => {
-    res.json({
-        url: "https://awesome-profile-cards.herokuapp.com/card/29111662374762259"
-    })
+server.get('/card/:id', (req, res) => {
+  res.json({
+    url: 'https://awesome-profile-cards.herokuapp.com/card/29111662374762259',
+  });
 });
-
