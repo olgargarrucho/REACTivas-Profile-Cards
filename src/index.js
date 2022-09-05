a0fbff6376bfc7790e845c1d0b109db239173bcc
Laura Martin gonzalez <lauramartingonzalez@MacBook-Pro-de-Laura.local>
2022-09-05 13:13:31 +0200

    install express cors nodemon and create endpoints

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const server = express();
+server.use(cors())
+server.use(express.json())
+const port = 4000;
+server.listen(port, () => {
+    console.log('listening' + port);
+})
+//endpoint
+server.post("/card", (req, res) => {
+    const responseSuccess = {
+        cardURL: "https://awesome-profile-cards.herokuapp.com/card/29111662374762259",
+        success: true
+    }
+    const responseError = {
+        success: false,
+        error: "Falta un parámetro"
+    }
+    res.json(responseError);
+});
+// endpoint "devolver tarjeta"
+server.get("/card/:id", (req, res) => {
+    res.json({
+        url: "https://awesome-profile-cards.herokuapp.com/card/29111662374762259"
+    })
+});
+
